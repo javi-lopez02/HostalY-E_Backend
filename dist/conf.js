1941665db25e40a19714773658eae8d2b96c02a9
javi-lopez02 <mjlphone0210@gmail.com>
2025-06-12 12:51:06 -0400

    update

--- dist/conf.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_URL = exports.TOKEN_SECRET = void 0;
-exports.TOKEN_SECRET = 'myPassword';
-exports.SERVER_URL = "http://localhost:4000";
